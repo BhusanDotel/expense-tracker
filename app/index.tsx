73fa592313fae844e45c0b00371648d8d2949a6b
Bhusan Dotel <bhusandotel1@gmail.com>
2025-10-21 13:24:20 +0545

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="bg-black flex items-center h-screen justify-center">
+      <View className="bg-blue-600 h-10 w-20" />
+    </View>
+  );
+}
